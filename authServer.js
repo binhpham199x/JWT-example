26df44428ef25ed6a5d7d947ed1f5a3c3a7edcff
Binh Pham <binh.pham@synaigy.com>
2023-11-06 16:53:44 +0100

    add refresh token logic and test request for new access token generating

--- authServer.js
+++ authServer.js
@@ -7,16 +7,40 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
+// this tokens should be saved in database
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAccessToken({ name: user.name });
+        res.json({ accessToken: accessToken });
+    })
+})
+
 app.post("/login", (req, res) => {
     // Authentication User
 
     const username = req.body.username;
     const user = { name: username };
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({ accessToken: accessToken });
+    const accessToken = generateAccessToken(user);
+
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+
+    res.json({
+        accessToken: accessToken,
+        refreshToken: refreshToken
+    });
 
 })
 
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+}
 
 app.listen(4000);
