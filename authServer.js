1ede5c2160973d9f0cc8f0290a6ea2cc3127154a
Binh Pham <binh.pham@synaigy.com>
2023-11-06 16:19:39 +0100

    remove unnecessary function in authServer.js file

--- authServer.js
+++ authServer.js
@@ -18,19 +18,5 @@ app.post("/login", (req, res) => {
 
 })
 
-function authenticateToken(req, res, next) {
-    // Example of authorization part of header: "authorization": "Bearer reugndsvbb23409thbsenfd..."
-
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    })
-}
 
 app.listen(4000);
