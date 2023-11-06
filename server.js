b815886f9a9df6e927917c4e149441618f6ecf3b
Binh Pham <binh.pham@synaigy.com>
2023-11-06 15:13:40 +0100

    correct writting mistakes

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.post("/login", (req, res) => {
     const username = req.body.username;
     const user = { name: username };
 
-    const acessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-    res.json({acessToken: acessToken});
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
 
 })
 
